11a76ae1172db413cb497d6193b4ad89518e3506
Ilja6104 <ilja.ham@gmail.com>
2018-07-25 03:18:02 +0200

    changed some terrain and added cart

---
+++ terrainTest/Assets/scripts/cartMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class cartMovement : MonoBehaviour {
+
+    public Transform target;
+
+    public float smoothSpeed = 0.25f;
+
+    private bool cartFollow = false;
+    public Vector3 offset;
+    public float distance;
+    public float distaneToGrab = 2.5f;
+
+    private void Update()
+    {
+        distance = Vector3.Distance(transform.position, target.transform.position);
+    }
+
+    void LateUpdate()
+    {
+
+
+        
+    }
+
+
+
+}
+
